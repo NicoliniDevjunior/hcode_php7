1a14bfd2a16b9a6b5f436651cdbaa685503409cb
Eduardo Nicolini <projetosdevjunior@gmail.com>
2022-11-11 18:17:15 -0300

    novas aulas

--- secao-9-poo/aula-42/exemplo-1.php
+++ secao-9-poo/aula-42/exemplo-1.php
@@ -1,23 +1,22 @@
 <?php
 
-class Pessoa { // Toda classe sempre vai começar com letra maiuscula
+    class Pessoa { // Toda classe sempre vai começar com letra maiuscula
 
-    public $nome; // Atributo
+        public $nome; // Atributo
 
-    public function falar() { // Método
-        /* Só dentro de um metodo que irei usar $this 
-        que é uma variavel interna do PHP 
-        que serve para referenciar atributos e metodos dentro de outros metodos
-        */
-        return "O meu nome é " .$this->nome; // Referencio o sem o $ nome o $this faz o papel do $
-    
+        public function falar() { // Método
+            /* Só dentro de um metodo que irei usar $this 
+            que é uma variavel interna do PHP 
+            que serve para referenciar atributos e metodos dentro de outros metodos
+            */
+            return "O meu nome é " .$this->nome; // Referencio o sem o $ nome o $this faz o papel do $
+        
+        }
+
     }
 
-}
-
-
-$marcus = new Pessoa(); // Objeto variavel oi instancia Pessoa() que representa uma classe posso usar o Pessoa sem o () no final tambem
-$marcus->nome = "Marcus Ribeiro"; // Referencio um atributo com o ->, só consigo acessar a classe e referenciar por que ele é publico
-echo $marcus->falar();
+    $marcus = new Pessoa(); // Objeto variavel oi instancia Pessoa() que representa uma classe posso usar o Pessoa sem o () no final tambem
+    $marcus->nome = "Marcus Ribeiro"; // Referencio um atributo com o ->, só consigo acessar a classe e referenciar por que ele é publico
+    echo $marcus->falar();
 
 ?>
--- secao-9-poo/aula-43/exemplo-2.php
+++ secao-9-poo/aula-43/exemplo-2.php
@@ -1,70 +1,71 @@
 <?php
 
-class Carro {
+    class Carro {
 
-    private $modelo;
-    private $motor;
-    private $ano;
-    
-    // Inicio com o get para pegar e Modelo com M maiusculo que é composto
-    public function getModelo() {
-
-        return $this->modelo; // $this = esse -> modolo ou objeto
-        // retorno o valor do atributo    
-    }
-
-    public function setModelo($modelo) {
+        private $modelo;
+        private $motor;
+        private $ano;
         
-        $this->modelo = $modelo;
-        // não preciso retornar atributo, se eu quiser chamar o atributo uso o metodo get
-        // o metoro set eu só passo o valor
+        // Inicio com o get para pegar e Modelo com M maiusculo que é composto
+        public function getModelo() {
+
+            return $this->modelo; // $this = esse -> modolo ou objeto
+            // retorno o valor do atributo    
+        }
+
+        public function setModelo($modelo) {
+            
+            $this->modelo = $modelo;
+            // não preciso retornar atributo, se eu quiser chamar o atributo uso o metodo get
+            // o metoro set eu só passo o valor
+        }
+
+        //get pego os valores
+        public function getMotor():float {
+
+            return $this->motor;
+
+        }
+
+        // set mudo os valores
+        public function setMotor($motor) {
+
+            $this->motor = $motor;
+
+        }
+
+        public function getAno(){
+
+            return $this->ano;
+
+        }
+
+        public function setAno($ano) {
+
+            $this->ano = $ano;
+
+        }
+
+        public function exibir(){
+
+            return array(
+
+                "modelo" => $this->getModelo(),
+                "motor" => $this->getMotor(),
+                "ano" => $this->getAno()
+
+            );
+
+        }
+
     }
 
-    //get pego os valores
-    public function getMotor():float {
+    $gol = new Carro();
+    $gol->setModelo("Gol GT");
+    $gol->setMotor("1.6");
+    $gol->setAno("2017");
 
-        return $this->motor;
-
-    }
-
-    // set mudo os valores
-    public function setMotor($motor) {
-
-        $this->motor = $motor;
-
-    }
-
-    public function getAno(){
-
-        return $this->ano;
-
-    }
-
-    public function setAno($ano) {
-
-        $this->ano = $ano;
-
-    }
-
-    public function exibir(){
-
-        return array(
-
-            "modelo" => $this->getModelo(),
-            "motor" => $this->getMotor(),
-            "ano" => $this->getAno()
-
-        );
-
-    }
-
-}
-
-$gol = new Carro();
-$gol->setModelo("Gol GT");
-$gol->setMotor("1.6");
-$gol->setAno("2017");
-
-var_dump($gol->exibir());
+    var_dump($gol->exibir());
+    
 
 ?>
